e50be7a9f719d682d7a11473cb6a8a75b8d1f809
Sarath Sreedharan <ssreedh3@asu.edu>
2024-09-29 23:21:56 -0600

    Removing print statements

--- Utils.py
+++ Utils.py
@@ -13,16 +13,6 @@ def ValueIteration(mdp, epsilon=0.001):
         for s in mdp.get_state_space():
             s_hash = mdp.get_state_hash(s)
             v = V[s_hash]
-            #print([([mdp.get_transition_probability(s, a, s_prime) for s_prime in mdp.get_state_space()]) for a in mdp.get_actions()])
-            for  a in mdp.get_actions():
-                #print("act",a)
-                value_for_a = 0
-                #prob_list = []
-                for s_prime in mdp.get_state_space():
-                    value_for_a += mdp.get_transition_probability(s, a, s_prime) * (mdp.get_reward(s, a, s_prime) + V[mdp.get_state_hash(s_prime)])
-                    #prob_list.append(mdp.get_transition_probability(s, a, s_prime))
-                #print(s[0],a, mdp.discount * value_for_a)
-                #print(s,a,prob_list)
             V[s_hash] = max([mdp.discount * sum([mdp.get_transition_probability(s, a, s_prime) * (mdp.get_reward(s, a, s_prime) + V[mdp.get_state_hash(s_prime)]) for s_prime in mdp.get_state_space()]) for a in mdp.get_actions()])
             delta = max(delta, abs(v - V[s_hash]))
         #print(delta)
